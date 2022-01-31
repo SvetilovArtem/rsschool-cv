558229fb78d994f98f6c90e98bf11ab05c267352
Svetilov Artem <smart12.12@mail.ru>
2022-02-01 00:08:53 +0300

    delete portfolio 1

--- portfolio#1/scripts/sidebar.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('.burger-btn');
-const navActive = document.querySelector('.nav-active');
-
-button.addEventListener('click', function() {
-    navActive.classList.toggle('is-hidden')
-})
